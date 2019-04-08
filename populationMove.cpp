98001fad768688c1fc274658001fb0f0f8eb23ed
YoungHwan <keum2003@naver.com>
2019-04-09 00:09:54 +0900

    인구이동 수정

--- populationMove.cpp
+++ populationMove.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <stdio.h>
 #include <memory.h>
+
+#define ABS(X) ((X) > 0? (X) : -(X))
 
 using namespace std;
 
@@ -8,43 +12,59 @@ int check[51][51] = { 0 };
 
 void bfs(int i, int j, int cnt, int n, int l, int r, int **arr) {
 	queue<pair<int, int>> location;
+	vector<pair<int, int>> cal;
 
 	int dx[4] = { 0,0,-1,1 };
 	int dy[4] = { 1,-1,0,0 };
 	location.push({ i,j });
+	cal.push_back({ i,j });
+	check[i][j] = cnt;
 	int x, y, nx, ny, minus;
-	
+	int sum = 0;
+	int count = 0;
+
 	while (!location.empty()) {
 		x = location.front().first;
 		y = location.front().second;
 		location.pop();
-		check[x][y] = cnt;
+		sum += arr[x][y];
+		count++;
+		//check[x][y] = cnt;
 
 		for (int i = 0; i < 4; i++) {
 			nx = x + dx[i];
 			ny = y + dy[i];
 
 			if ((nx >= 0) && (nx < n) && (ny >= 0) && (ny < n) && check[nx][ny]==0) {
-				minus = arr[x][y] - arr[nx][ny];
-				minus = minus >= 0 ? minus : minus * (-1);
-				if (minus >= l && minus <= r) {
+				
+				if (l <= ABS(arr[x][y] - arr[nx][ny]) && ABS(arr[x][y] - arr[nx][ny]) <= r) {
 					location.push({ nx, ny });
-					//check[nx][ny] = cnt;
+					cal.push_back({ nx, ny });
+					check[nx][ny] = cnt;
+					//sum += arr[nx][ny];
+					//count++;
 				}
 					
 			}
 		}
 	}
+	for (int i = 0; i < cal.size(); i++) {
+		arr[cal[i].first][cal[i].second] = sum / count;
+	}
 }
 
 void searchLine(int **arr, int l, int r, int n) {
 	int total = 0;
+	int i, j, k;
+	int cnt;
+	int countOfCnt;
+	int sum;
 
 	while (1) {
 		
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+		cnt = 0;
+		for ( i = 0; i < n; i++) {
+			for ( j = 0; j < n; j++) {
 				if (check[i][j] == 0)
 					bfs(i, j, ++cnt,  n, l, r, arr);
 			}
@@ -54,19 +74,19 @@ void searchLine(int **arr, int l, int r, int n) {
 			break;
 		}
 
-		for (int i = 1; i <= cnt; i++) {
-			int countOfCnt = 0;
-			int sum = 0;
-			for (int k = 0; k < n; k++)
-				for (int j = 0; j < n; j++) {
+		/*for (i = 1; i <= cnt; i++) {
+			countOfCnt = 0;
+			sum = 0;
+			for ( k = 0; k < n; k++)
+				for ( j = 0; j < n; j++) {
 					if (check[k][j] == i) {
 						countOfCnt++;
 						sum += arr[k][j];
 					}
 				}
 
-			for (int k = 0; k < n; k++) {
-				for (int j = 0; j < n; j++) {
+			for ( k = 0; k < n; k++) {
+				for ( j = 0; j < n; j++) {
 					if (check[k][j] == i) {
 						arr[k][j] = sum / countOfCnt;
 					}
@@ -74,9 +94,12 @@ void searchLine(int **arr, int l, int r, int n) {
 			}
 			sum = 0;
 			countOfCnt = 0;
-		}
+		}*/
 		total++;
-		memset(check, 0, sizeof(check));
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+				check[i][j] = 0;
+		//memset(check, 0, sizeof(check));
 	}
 }
 
@@ -90,16 +113,13 @@ int main() {
 		arr[i] = new int[n];
 		//check[i] = new int[n];
 	}
-		
 
-	int input;
 	for(int i=0; i<n; i++)
 		for (int j = 0; j < n; j++) {
-			cin >> input;
-			arr[i][j] = input;
+			cin >> arr[i][j];
 			//check[i][j] = 0;
 		}
 
-	searchLine(arr, l, r, n);
+	searchLine(arr,  l, r, n);
 
 }
