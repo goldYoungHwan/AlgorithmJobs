639922430a113284e8cd88d8aab72030198aa710
YoungHwan <keum2003@naver.com>
2019-05-03 12:33:02 +0900

    369게임

---
+++ SWEA/D2/369game.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int i, int count) {
+	if (count == 0)
+		cout << i << " ";
+	else if (count == 1)
+		cout << "-" << " ";
+	else if (count == 2)
+		cout << "--" << " ";
+	else if (count == 3)
+		cout << "---" << " ";
+}
+int second(int i) {
+	int div;
+	int rest;
+	int count = 0;
+
+	div = i / 10;
+	rest = i % 10;
+
+	if (div != 0 && div % 3 == 0)
+		count++;
+
+	if (rest != 0 && rest % 3 == 0)
+		count++;
+
+	return count;
+}
+int first(int i) {
+
+	int count = 0;
+	if (i % 3 == 0) {
+		count++;
+	}
+
+	return count;
+}
+
+int main() {
+	int n;
+	int div, rest;
+	int count = 0;
+
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+		if (i >= 1 && i <= 9) {
+			count = first(i);
+			print(i, count);
+		}
+
+		if (i >= 10 && i <= 99) {
+			count = second(i);
+			print(i, count);
+		}
+		if (i >= 100 && i <= 999) {
+			div = i / 100;
+			rest = i % 100;
+			count = first(div);
+			count += second(rest);
+			print(i, count);
+		}
+	}
+	cout << endl;
+}
